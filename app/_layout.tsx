8f3dbec14c22ae7f91199dd367994d889b2b06d8
Harry Keys <harry.keys40@gmail.com>
2024-12-12 19:31:32 +1100

    #17 Navigation Bar Moved files around to get default pathing set up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,11 +4,10 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="pages/feed/FeedPage" options={{ title: 'Feed' }} />
-      <Tabs.Screen name="pages/matches/MatchesPage" options={{ title: 'Matches' }} />
-      <Tabs.Screen name="pages/messages/MessagesPage" options={{ title: 'Messages' }} />
-      <Tabs.Screen name="pages/profile/ProfilePage" options={{ title: 'Profile' }} />
-      <Tabs.Screen name="pages/settings/SettingsPage" options={{ title: 'Settings' }} />
+      <Tabs.Screen name="tabs/matchesPage" options={{ title: 'Matches' }} />
+      <Tabs.Screen name="tabs/messagesPage" options={{ title: 'Messages' }} />
+      <Tabs.Screen name="index" options={{ title: 'Feed' }} />
+      <Tabs.Screen name="tabs/profilePage" options={{ title: 'Profile' }} />
     </Tabs>
   );
 }
