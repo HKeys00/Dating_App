427f6f9785e6f1ac61b652ba8a46aad1f4ac842a
Harry Keys <harry.keys40@gmail.com>
2025-02-04 18:06:37 +1100

    feed page done started on chat page

--- app/pages/chatPage.tsx
+++ app/pages/chatPage.tsx
@@ -1,27 +1,56 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
 import { useSearchParams } from 'expo-router/build/hooks';
 import { useNavigation } from 'expo-router';
-import { useRoute } from '@react-navigation/native';
-import Page from './base/page';
+import chatPageTheme from '../../styles/pages/chatPageTheme';
+import dummyChats from '../../data/chatData.json';
+import ChatBubble from '@/components/chat/chatBubble';
+import MessageType from '@/types/messaging/messageType';
 
 export const options = {
   tabBarStyle: { display: 'none' }, // Hide the tab bar for this screen
 };
 
 const ChatPage: React.FC = () => {
+  const [messages, setMessages] = useState<MessageType[]>([]);
+  const [loading, setLoading] = useState(false); // To manage loading state
+
   const searchParams = useSearchParams();
   const chatId = searchParams.get('chatId');
   const navigation = useNavigation();
-  const route = useRoute();
 
   useEffect(() => {
     navigation.setOptions({ headerShown: false });
+
+    if (!chatId) return;
+    setMessages(dummyChats.messages);
   }, [navigation]);
 
+  const loadMoreMessages = async () => {
+    if (loading) return;
+    setLoading(true);
+    setTimeout(() => {
+      const newMessages = [
+        { id: 9, senderId: 1, message: 'Older message 1', timestamp: '2025-01-28T08:30:00Z' },
+        { id: 10, senderId: 2, message: 'Older message 2', timestamp: '2025-01-28T08:31:00Z' },
+      ];
+
+      setMessages((prevMessages) => [...newMessages, ...prevMessages]);
+      setLoading(false);
+    }, 1500);
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} onTouchEnd={() => navigation.goBack()}>
-      <Text>Chat ID: {chatId}</Text>
+    <View style={chatPageTheme.page}>
+      <FlatList
+        data={messages}
+        inverted // Makes the list scroll from bottom to top
+        renderItem={({ item, index }) => <ChatBubble message={item} key={item.id} />}
+        keyExtractor={(item) => item.id.toString()}
+        onStartReached={loadMoreMessages} // Load more messages when scrolled to the top
+        onStartReachedThreshold={0.1} // Trigger when 10% of the list is scrolled to the top
+        ListFooterComponent={loading ? <ActivityIndicator size="large" color="blue" /> : null} // Show loading spinner
+      />
     </View>
   );
 };
